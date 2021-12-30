7784601acd1f5772f05ce87d451535b13306ad18
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-30 11:54:46 +0100

    Add next router in cars component

--- src/pages/cars.tsx
+++ src/pages/cars.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const CarListPage = () => {
+  const router = useRouter();
+
+  const onNavigateBack = () => {
+    router.push('/'); // router.back();
+  };
+
   return (
     <>
       <h2>Car list page</h2>
@@ -8,6 +15,7 @@ const CarListPage = () => {
         <li>Audi Q8</li>
         <li>BMW X7</li>
       </ul>
+      <button onClick={onNavigateBack}>Navigate to home</button>
     </>
   );
 };
