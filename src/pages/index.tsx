5a7a12dcc59fe7ba7f87ec2866eabe1b2460a670
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-30 12:22:42 +0100

    Change page title using Head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head"; 
 
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Rent a car - Home</title>
+      </Head>
       <h2>Hello from Nextjs</h2>
       <Link href="/cars">Navigate to car list</Link>
     </>
