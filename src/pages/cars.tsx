a727d41f9ace55b2c5e54dcb80aec7846e4dfae6
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-30 11:47:29 +0100

    Create component cars

---
+++ src/pages/cars.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CarListPage = () => {
+  return (
+    <>
+      <h2>Car list page</h2>
+      <ul>
+        <li>Audi Q8</li>
+        <li>BMW X7</li>
+      </ul>
+    </>
+  );
+};
+
+export default CarListPage;
