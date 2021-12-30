cb9933f4f5defb38b0a839f890a9d1584ff28ca3
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-30 11:50:31 +0100

    Add Link to navigate though component details

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <h2>Hello from Nextjs</h2>
-}
+  return (
+    <>
+      <h2>Hello from Nextjs</h2>
+      <Link href="/cars">Navigate to car list</Link>
+    </>
+  );
+};
 
-export default HomePage; 
+export default HomePage;
+
